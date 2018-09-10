b9fde098e7308890e18a0844ca626c31016f5057
Manish4586 <Manish.n.manish45@gmail.com>
2018-09-10 15:24:51 +0530

    /include/uapi/linux/netfilter
    Fix Build Error Due To Missing Header.
    
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

---
+++ include/uapi/linux/netfilter/xt_mark.h
@@ -0,0 +1,18 @@
+/* 
+From: Manish4586 <manish.n.manish45@gmail.com>
+*/
+#ifndef _XT_MARK_H
+#define _XT_MARK_H
+
+#include <linux/types.h>
+
+struct xt_mark_tginfo2 {
+	__u32 mark, mask;
+};
+
+struct xt_mark_mtinfo1 {
+	__u32 mark, mask;
+	__u8 invert;
+};
+
+#endif /*_XT_MARK_H*/
